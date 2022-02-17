dc80da7871e0b5324b1aabbe84c168f3edb5a65b
Eli Davidson <eli@shadowlab.dev>
2022-02-16 17:44:32 -0700

    Refactor

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-/*  REQUIREMENTS
-##Expectations
-[ ] Complete iteration 1 and at least one CYOA feature. 
-
-##Iteration 0: Build Out Comp
-HTML
-[x] fill out the form
-[x] Create page structure
-[x] Create Nav bar, including one button
-[x] Create main section, including background image
-[x] Create box 1, including title, radio buttons, and a button.
-[x] Create box 2 and with svg space and a space to display two lines of text.
-CSS
-[x] create colors, and classes to match the HTML structure.
-
-##Iteration 1: Minimum Viable Product (MVP) - Add Random Side, Main and Dessert Functionality
-[x] (if possible) disable entire meal radio button
-[x] add the js data structure.
-[x] add event listener for the lets cook button.
-[x] create a randomize array elements function
-[x] create a show function.
-[x] create a hide function.
-[ ] create a show meal helper method
-[ ] create a hide meal helper method skelaton
-[ ] create a show crockpot helper method.
-[ ] create a hide crockpot helper method.
-[ ] Connect everything.
-
-CYOA - Error Handling and Clear Button
-[ ]  add event listener for the clear button.
-[ ] User should only be able to click the clear button if a food is visible. 
-[ ] When the clear button is clicked and the food is removed, the image of the cookpot should re-appear.
-[ ] User should not be able to click the [“Let’s Cook” button for a recipe unless they have selected an option.
-*/
-
 var application = {
     mealData: {
         sides: [
@@ -149,9 +114,13 @@ var application = {
     }
 }
 
-var keys = Object.keys(application);
+window.addEventListener('load', initializeApplication)
 
-for (var i = 1; i < keys.length; i++) {
-    application[keys[i]].setEventListeners();
+function initializeApplication() {
+    var keys = Object.keys(application);
+    for (var i = 1; i < keys.length; i++) {
+        application[keys[i]].setEventListeners();
+    }  
 }
+
 
