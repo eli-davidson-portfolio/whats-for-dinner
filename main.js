573e6eeb4799fa8f364ca06ba7828b0d83211b53
Eli Davidson <eli@shadowlab.dev>
2022-02-17 20:16:12 -0700

    Fix typos

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
             "Crispy Potatoes",
             "Sweet Potato Tots",
             "Coconut Rice",
-            "Caeser Salad",
+            "Caesar Salad",
             "Shrimp Summer Rolls",
             "Garlic Butter Mushrooms",
             "Hush Puppies"
